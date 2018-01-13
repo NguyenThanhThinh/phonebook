2e344993b0c9a0bbcee75f3cd7a3c4ac03248d65
Thanh Thinh <thanhthinhcntt@gmail.com>
2018-01-13 21:09:04 +0700

    add login and register

--- phonebook/phonebook/Controllers/AccountController.cs
+++ phonebook/phonebook/Controllers/AccountController.cs
@@ -6,9 +6,101 @@ using System.Web.Mvc;
 
 namespace phonebook.Controllers
 {
+    using phonebook.ViewModels.Account;
+    using phonebook.Services;
+    using phonebook.Repositories;
+    using phonebook.Models;
+
     public class AccountController : Controller
     {
-        // GET: Account
+        public ActionResult Login(string RedirectUrl)
+        {
+            return View(new LoginViewModel
+            {
+                RedirectUrl=RedirectUrl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            AuthenticationService.AuthenticateUser(model.Username, model.Password);
+
+            if (AuthenticationService.IsLogged)
+            {
+                if (string.IsNullOrWhiteSpace(model.RedirectUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return Redirect(model.RedirectUrl);
+            }
+
+            return View();
+
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationService.LogOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(AccountRegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserRepository userRepo = new UserRepository();
+
+                User user = new User()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Password = model.Password
+                };
+
+                bool userDoesntExist = true;
+
+                User userUsername = userRepo.GetAll(us => us.Username == model.Username).FirstOrDefault();
+
+                if (userUsername != null)
+                {
+                    ModelState.AddModelError("Username", "Username already exists");
+
+                    userDoesntExist = false;
+                }
+
+                user.Username = model.Username;
+
+                User userEmail = userRepo.GetAll(us => us.Email == model.Email).FirstOrDefault();
+
+                if (userEmail != null)
+                {
+                    ModelState.AddModelError("Email", "Email already exists");
+
+                    userDoesntExist = false;
+                }
+
+                user.Email = model.Email;
+
+                if (userDoesntExist)
+                {
+                    userRepo.Save(user);
+
+                    return RedirectToAction("Login");
+                }
+            }
+
+            return View(model);
+        }
         public ActionResult Index()
         {
             return View();
---
+++ phonebook/phonebook/ViewModels/Account/AccountProfileViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace phonebook.ViewModels.Account
+{
+    public class AccountProfileViewModel:BaseViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 2)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Repeat Password")]
+        [Compare("Password")]
+        public string RepeatPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase Image { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public string ImageName { get; set; }
+    }
+}
