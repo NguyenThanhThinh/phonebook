d98825f9f74530b7ce52575875e8ee65c419fcf6
Thanh Thinh <thanhthinhcntt@gmail.com>
2018-01-13 22:38:31 +0700

    add baselistvm

---
+++ phonebook/phonebook/ViewModels/BaseListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Web.Routing;
+
+namespace phonebook.ViewModels
+{
+    public class BaseListViewModel<Entity>
+    {
+        public List<Entity> Entities { get; set; }
+
+        public RouteValueDictionary RouteDictionary { get; set; }
+    }
+}
