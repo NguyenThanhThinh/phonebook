1e732c6b6a9fbdb6be1f79da6babd8ec38734f63
Thanh Thinh <thanhthinhcntt@gmail.com>
2018-01-14 00:55:19 +0700

    update

--- phonebook/phonebook/Controllers/UserController.cs
+++ phonebook/phonebook/Controllers/UserController.cs
@@ -11,15 +11,20 @@ namespace phonebook.Controllers
     using phonebook.Models;
     using PagedList;
     using System.Web.Routing;
+    using System.IO;
+    using phonebook.Services;
 
     public class UserController : Controller
     {
+        private readonly UserRepository userRepo;
 
+        public UserController()
+        {
+            userRepo = new UserRepository();
+        }
         public ActionResult Index()
         {
             UserListViewModel model = new UserListViewModel();
-
-            UserRepository userRepo = new UserRepository();
 
             TryUpdateModel(model);
 
@@ -96,7 +101,256 @@ namespace phonebook.Controllers
 
         public ActionResult Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            User user = userRepo.GetById(id.Value);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            UserDetailsViewModel userDetail = new UserDetailsViewModel()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username,
+                Email = user.Email
+            };
+
+            if (user.ImageName != null)
+            {
+                userDetail.ImageName = user.ImageName;
+            }
+
+            return View(userDetail);
         }
+
+        public ActionResult CreateEdit(int? id)
+        {
+            if (id == null) // Create
+            {
+                return View(new UserCreateEditViewModel());
+            }
+
+            if (id > 0) // Edit
+            {
+                UserRepository userRepo = new UserRepository();
+
+                User user = userRepo.GetById(id.Value);
+
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+
+                UserCreateEditViewModel u = new UserCreateEditViewModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Username = user.Username,
+                    Email = user.Email,
+                    IsAdmin = user.IsAdmin
+                };
+
+                if (user.ImageName != null)
+                {
+                    u.ImageName = user.ImageName;
+                }
+
+                return View(u);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEdit(UserCreateEditViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user;
+
+
+            if (model.Id > 0) // Edit
+            {
+                user = userRepo.GetById(model.Id);
+
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+            }
+
+            else // Create
+            {
+                user = new User();
+            }
+
+            if (model.Image != null && model.Image.ContentLength > 0)
+            {
+                if (model.Image.ContentType.Contains("image"))
+                {
+                    model.ImageName = Path.GetFileName(model.Image.FileName);
+
+                    model.ImageName = model.Username + Path.GetExtension(model.ImageName);
+
+                    string imagePath = Server.MapPath("~/Images/" + model.ImageName);
+
+                    model.Image.SaveAs(imagePath);
+
+                    user.ImageName = model.ImageName;
+                }
+
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Uploaded file isn't an image!");
+                    return View(model);
+                }
+            }
+
+            user.FirstName = model.FirstName;
+
+            user.LastName = model.LastName;
+
+            user.Password = model.Password;
+
+            user.IsAdmin = model.IsAdmin;
+
+            bool userDoesntExist = true;
+
+            if (model.Username != user.Username)
+            {
+                User userUsername = userRepo.GetAll(us => us.Username == model.Username).FirstOrDefault();
+
+                if (userUsername != null)
+                {
+                    ModelState.AddModelError("Username", "Username already exists");
+
+                    userDoesntExist = false;
+                }
+
+                user.Username = model.Username;
+            }
+
+            if (model.Email != user.Email)
+            {
+                User userEmail = userRepo.GetAll(us => us.Email == model.Email).FirstOrDefault();
+
+                if (userEmail != null)
+                {
+                    ModelState.AddModelError("Email", "Email already exists");
+
+                    userDoesntExist = false;
+                }
+
+                user.Email = model.Email;
+            }
+
+            if (userDoesntExist)
+            {
+                userRepo.Save(user);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null || AuthenticationService.LoggedUser.Id == id)
+            {
+                return RedirectToAction("Index");
+            }
+
+            UserRepository userRepo = new UserRepository();
+
+            User user = userRepo.GetById(id.Value);
+
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            UserDetailsViewModel u = new UserDetailsViewModel()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Username = user.Username,
+                Email = user.Email
+            };
+
+            if (user.ImageName != null)
+            {
+                u.ImageName = user.ImageName;
+            }
+
+            return View(u);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            UserRepository userRepo = new UserRepository();
+
+            User user = userRepo.GetById(id.Value);
+
+            if (id != AuthenticationService.LoggedUser.Id)
+            {
+                userRepo.Delete(user);
+
+                if (user.ImageName != null)
+                {
+                    string imagePath = Request.MapPath("~/Images/" + user.ImageName);
+
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+                }
+            }
+
+            else
+            {
+                return HttpNotFound();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeletePicture(string imageName, int? id)
+        {
+            string imagePath = Request.MapPath("~/Images/" + imageName);
+
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+
+                UserRepository userRepo = new UserRepository();
+
+                User user = userRepo.GetById(id.Value);
+
+                user.ImageName = null;
+
+                userRepo.Save(user);
+
+                return RedirectToAction("CreateEdit", new { id = id.Value });
+            }
+
+            return HttpNotFound();
+        }
+
     }
 }
