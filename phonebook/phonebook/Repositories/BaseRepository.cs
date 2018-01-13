12699e542bea5fd18ae700975d75731d662c2314
Thanh Thinh <thanhthinhcntt@gmail.com>
2018-01-13 20:29:57 +0700

    add base repository

---
+++ phonebook/phonebook/Repositories/BaseRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace phonebook.Repositories
+{
+    using phonebook.Models;
+    using System.Data.Entity;
+    using System.Linq.Expressions;
+
+    public class BaseRepository<T> where T : BaseEntity
+    {
+        public DbContext Context { get; set; }
+
+        public DbSet<T> DbSet { get; set; }
+
+        public BaseRepository()
+        {
+            Context = new PhonebookDbcontext();
+
+            DbSet = Context.Set<T>();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> result = DbSet;
+
+            if (filter != null)
+            {
+                return result.Where(filter).ToList();
+            }
+            else
+            {
+                return result.ToList();
+            }
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Save(T entity)
+        {
+            if (entity.Id > 0)
+            {
+                Update(entity);
+            }
+            else
+            {
+                Insert(entity);
+            }
+        }
+
+        private void Insert(T entity)
+        {
+            DbSet.Add(entity);
+            Context.SaveChanges();
+        }
+
+        private void Update(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+            Context.SaveChanges();
+        }
+    }
+}
